fd3daebbb1f8b28f236676859d63ff8278d4e205
semteo04 <semteo04@naver.com>
2019-04-04 23:42:05 +0900

    change img

--- Mpaint.js
+++ Mpaint.js
@@ -1,35 +1,44 @@
 let c = document.getElementById('can');
 let ctx = c.getContext("2d");
 function dboard() {
-    ctx.beginPath();
-    ctx.strokeStyle = "gray";
-    ctx.lineWidth = 3;
-    ctx.fillStyle = "rgb(88, 88, 88)";
+    ctx.strokeStyle = "#666666";
+    ctx.lineWidth = 1;
+    ctx.fillStyle = "#E0E0E0";
     for (let i = 1; i <= 8; i += 2) {
         for (let j = 1; j <= 8; j += 2) {
+            ctx.beginPath(); 
             ctx.rect(-50 + i * 70, -50 + j * 70, 70, 70);
             ctx.fill();
             ctx.stroke();
+            ctx.closePath()
         }
     }
     for (let i = 2; i <= 8; i += 2) {
         for (let j = 2; j <= 8; j += 2) {
+            ctx.beginPath();
             ctx.rect(-50 + i * 70, -50 + j * 70, 70, 70);
             ctx.fill();
             ctx.stroke();
+            ctx.closePath()
         }
     }
     ctx.fillStyle = "white";
     for (let i = 2; i <= 8; i += 2) {
         for (let j = 1; j <= 8; j += 2) {
+            ctx.beginPath();
             ctx.rect(-50 + i * 70, -50 + j * 70, 70, 70);
+            ctx.fill();
             ctx.stroke();
+            ctx.closePath()
         }
     }
     for (let i = 1; i <= 8; i += 2) {
         for (let j = 2; j <= 8; j += 2) {
+            ctx.beginPath();
             ctx.rect(-50 + i * 70, -50 + j * 70, 70, 70);
+            ctx.fill();
             ctx.stroke();
+            ctx.closePath()
         }
     }
     ctx.closePath();
