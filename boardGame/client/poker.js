aa79fe8663eb0e50f754d2a1e9af792fc3747610
semteo04 <semteo04@naver.com>
2020-10-01 22:11:06 +0900

    통신 오류 수정

--- boardGame/client/poker.js
+++ boardGame/client/poker.js
@@ -1,8 +1,8 @@
-let wsUri = "ws://ec2-35-165-46-164.us-west-2.compute.amazonaws.com:3000";
+let wsUri = "wss://ec2-35-165-46-164.us-west-2.compute.amazonaws.com:3000";
 //let wsUri = "ws://localhost:3000";
 let output;
 
-let socket = io(wsUri);
+let socket = io(wsUri,{secure: true});
 socket.on("hello",(data) => {console.log(data);});
 socket.on("chat",(data) => {
     $('#chatting').append(`<li>${data}</li>`);
